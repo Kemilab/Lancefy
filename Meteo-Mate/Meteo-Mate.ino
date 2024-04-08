161a916b6afc3990510b1e1ed62e4d949868ea50
Kemilab <lukasalaj9@gmail.com>
2024-04-08 22:10:16 +0200

    Added Display For Lancefy

--- Meteo-Mate/Meteo-Mate.ino
+++ Meteo-Mate/Meteo-Mate.ino
@@ -5,29 +5,28 @@
 
 #include <WiFi.h>
 #include "secrets.h"
-#include "sendData.h"
 #include <Adafruit_BME680.h>
+#include <Wire.h>
 
 const char *ssid = WiFissid;
 const char *password = WiFipassword;
+const char *key = key;
 
 Adafruit_BME680 bme;
 
 void setup() {
   Serial.begin(115200);
+}
+void loop() {
+  readBME680();
+  delay(300);
+}
 
-  WiFi.begin(ssid, password);
-  Serial.println("WiFi setup started");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi!");
-  }
-  Serial.println("Connected to the network with IP address: ");
-  Serial.print(WiFi.localIP());
-
+void readBME680() {
+  // Read sensor data
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1);
+    while(1);
   }
 
   bme.setTemperatureOversampling(BME680_OS_8X);
@@ -35,17 +34,23 @@ void setup() {
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150);
-}
-void loop() {
 
-  // Read sensor data
   float temperature = bme.temperature;
   float humidity = bme.humidity;
   float pressure = bme.pressure / 100.0;              // Convert to hPa
   float gasResistance = bme.gas_resistance / 1000.0;  // Convert to kOhms
 
-  String jsonPayload = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + ",\"pressure\":" + String(pressure) + ",\"gasResistance\":" + String(gasResistance) + "}";
-  // Send data to dash.esclone.com
-  sendData(jsonPayload);
-  delay(5000);
+  Serial.print("Printing values");
+  Serial.println(String("Temperature is ") + String(temperature));
+  Serial.println(String("Humidity is ") + String(humidity));
+  Serial.println(String("Pressure is ") + String(pressure));
+  Serial.println(String("gasResistance is ") + String(gasResistance));
+}
+
+void read_UV() {
+  //todo
+}
+
+void wind_speed() {
+  //todo
 }
